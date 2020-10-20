74c9c22a2c5cc0adf4ca393b3f35ceb95c42976e
ericr491 <57604698+ericr491@users.noreply.github.com>
2020-10-19 21:16:57 -0400

    Add files via upload

---
+++ part3/3a/build/precache-manifest.1f341c6c2d5844c79cf15981876afdb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5233342ddc414c5312625aef74ca1f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afbe4a44b46eb7425501",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "99b567cd82da670379e2",
+    "url": "/static/js/2.78dc8e49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78dc8e49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afbe4a44b46eb7425501",
+    "url": "/static/js/main.9459b835.chunk.js"
+  },
+  {
+    "revision": "45bcc1bb98323ca61afa",
+    "url": "/static/js/runtime-main.2b26bbb1.js"
+  }
+]);
